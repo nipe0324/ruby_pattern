7b5b830632004de1739b42774c4f455bb9d41bca
Shojiro Yanagisawa <nipe0324@gmail.com>
2021-09-26 07:29:27 +0900

    Update interactor README.md

--- interactor/README.md
+++ interactor/README.md
@@ -6,3 +6,31 @@ Implemente business logic and return success or not and some objects easily.
 
 * [Hanami::Utils](https://github.com/hanami/utils)
 * [Interactor](https://github.com/collectiveidea/interactor)
+
+## Example
+
+```rb
+# Implement business logic with `Interactor`
+class AuthenticateUser
+  include Interactor
+
+  def call(email:, password:)
+    error!('invalid parameters') unless valid_params(email, password)
+
+    user = authenticate(email, password)
+    if user
+      result.user = user
+    else
+      error!('user authentication is failed')
+    end
+  end
+
+  # ...
+end
+
+# Caller handles usefull result.
+result = AuthenticateUser.call(email: 'john@example.com', password: 'password')
+result.successful? #=> true
+result.user        #=> #<struct User name="john", email="john@example.com">
+result.errors      #=> []
+```
--- interactor/main.rb
+++ interactor/main.rb
@@ -1,8 +1,10 @@
 require_relative 'interactor'
 
+User = Struct.new(:name, :email)
+
 class AuthenticateUser
   include Interactor
-  
+
   def call(email:, password:)
     error!('invalid parameters') unless valid_params(email, password)
 
@@ -21,18 +23,21 @@ class AuthenticateUser
   end
 
   def authenticate(email, password)
-    if email == 'john@example.com' && password == 'password'
-      { name: 'john', email: 'john@example.com' }
-    else
-      false
-    end
+    user_store.find { |user| email == user.email && password == 'password' } || false
+  end
+
+  def user_store
+    @user_store ||= [
+      User.new('john', 'john@example.com')
+    ]
   end
 end
 
 # success auth
 result = AuthenticateUser.call(email: 'john@example.com', password: 'password')
 p result.successful? #=> true
-p result.user #=> {:name=>"john", :email=>"john@example.com"}
+p result.user #=> #<struct User name="john", email="john@example.com">
+p result.errors #=> []
 
 # failed auth
 result = AuthenticateUser.call(email: 'john@example.com', password: 'invalid')
